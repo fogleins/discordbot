6554abc18f587d1bc688216ec74967065cb2238f
44982432 <fogleins@hotmail.hu>
2019-12-06 20:15:00 +0100

    Formatting events.py Changed ' to " in strings Removed and added spaces in some cases to match Python's formatting guide

--- cogs/events.py
+++ cogs/events.py
@@ -15,29 +15,28 @@ class Events(commands.Cog):
         simiid = '<@358992693453652000>'
         uzenet = f"{member.name} ({member.id}) is now part of this discord server!"
         embed = discord.Embed(
-            title=f'``{now}:`` ', 
-            description=f'{uzenet}', 
+            title=f'``{now}:`` ',
+            description=f'{uzenet}',
             colour=discord.Colour.magenta()
         )
         embed.set_thumbnail(url=member.avatar_url)
         embed.set_author(name=member.name + ' has joined the server', icon_url=member.avatar_url)
-        embed.add_field(name = 'Hey,', value = '{} you need to see this!'.format(simiid))
+        embed.add_field(name='Hey,', value=f"{simiid} you need to see this!")
         await self.bot.get_channel(550724640469942285).send(embed=embed)
-
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
         now = datetime.datetime.now()
         simiid = '<@358992693453652000>'
-        uzenet = "{} ({}) is now part of this discord server!".format(member.name, member.id)
+        uzenet = f"{member.name} ({member.id}) is now part of this discord server!"
         embed = discord.Embed(
-            title=f'``{now}:``', 
-            description=f'{uzenet}', 
+            title=f"``{now}:``",
+            description=f"{uzenet}",
             colour=discord.Colour.dark_magenta()
         )
         embed.set_thumbnail(url=member.avatar_url)
-        embed.set_author(name=member.name + ' has joined the server', icon_url=member.avatar_url)
-        embed.add_field(name = 'Hey,', value = '{} you need to see this!'.format(simiid))
+        embed.set_author(name=member.name + " has joined the server", icon_url=member.avatar_url)
+        embed.add_field(name='Hey,', value = f"{simiid} you need to see this!")
         await self.bot.get_channel(550724640469942285).send(embed=embed)
 
     @commands.Cog.listener()
@@ -53,17 +52,17 @@ class Events(commands.Cog):
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
         now = datetime.datetime.now()
-        if ((after.author.bot == False) and (before.content != after.content)):
+        if (after.author.bot is False) and (before.content != after.content):
             uzenet = (f"The message **{before.content}** has been edited to **{after.content}**" +
                 f" by **{after.author.name}** in channel {after.channel.mention}")
             embed = discord.Embed(
-                title='``{}:`` '.format(now),
-                description='{}'.format(uzenet),
+                title=f"``{now}:`` ",
+                description=f"{uzenet}",
                 colour=discord.Colour.orange()
             )
             embed.set_thumbnail(url=after.author.avatar_url)
             embed.set_author(name=f'{after.author.name} has edited a message', icon_url=after.author.avatar_url)
-            await self.bot.get_channel(550724640469942285).send(embed = embed)
+            await self.bot.get_channel(550724640469942285).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_message_delete(self, message):
@@ -71,16 +70,16 @@ class Events(commands.Cog):
         kiirhato = False
         uzenet_content = message.content
         if (message.author.id == 549654750585421825 or uzenet_content.startswith('?')
-            or uzenet_content.startswith('Unloaded') or uzenet_content.startswith('Loaded') 
-            or uzenet_content.startswith('User limit') or len(uzenet_content) > 1000 
+            or uzenet_content.startswith('Unloaded') or uzenet_content.startswith('Loaded')
+            or uzenet_content.startswith('User limit') or len(uzenet_content) > 1000
             or uzenet_content.startswith('\\') or uzenet_content.startswith('/////')):
                 pass
         else:
             if message.attachments:
                 try:
                     attachment_link = message.attachments[0].url
-                    logba_ez_megy = (f'The following message was deleted from {message.channel.mention}:' + 
-                        f'{attachment_link}')
+                    logba_ez_megy = (f"The following message was deleted from {message.channel.mention}:" +
+                        f"{attachment_link}")
                     kiirhato = True
                 except discord.DiscordException:
                     pass
@@ -90,15 +89,15 @@ class Events(commands.Cog):
                 logba_ez_megy = f'The message **{message.content}** was deleted from {message.channel.mention}'
                 kiirhato = True
             embed = discord.Embed(
-                title='``{}:`` '.format(now),
-                description='{}'.format(logba_ez_megy),
+                title=f"``{now}:`` ",
+                description=f"{logba_ez_megy}",
                 colour=discord.Colour.red()
             )
             embed.set_thumbnail(url=message.author.avatar_url)
-            embed.set_author(name = f'A message sent by {message.author.name} was deleted', 
+            embed.set_author(name=f"A message sent by {message.author.name} was deleted",
                 icon_url=message.author.avatar_url)
             if kiirhato:
-                await self.bot.get_channel(550724640469942285).send(embed = embed)
+                await self.bot.get_channel(550724640469942285).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
@@ -109,7 +108,7 @@ class Events(commands.Cog):
         else:
             if reaction.message.attachments:
                 try:
-                    #attachment_link = reaction.message.attachments[0]['url']
+                    # attachment_link = reaction.message.attachments[0]['url']
                     attachment_link = reaction.message.attachments[0].url
                     kiirando_uzenet = (f"{user.name} has added {reaction.emoji} to" 
                         f" {reaction.message.author}'s message in channel {reaction.message.channel.mention}"+
@@ -125,14 +124,14 @@ class Events(commands.Cog):
                     f" {reaction.message.content}")
                 kiirhato = True
             embed = discord.Embed(
-                title='``{}:`` '.format(now), 
-                description='{}'.format(kiirando_uzenet), 
+                title=f"``{now}:`` ",
+                description=f"{kiirando_uzenet}",
                 colour=discord.Colour.teal()
             )
             embed.set_thumbnail(url=reaction.message.author.avatar_url)
             embed.set_author(name=user.name + ' has added a reaction', icon_url=user.avatar_url)
             if kiirhato:
-                await self.bot.get_channel(550724640469942285).send(embed = embed)
+                await self.bot.get_channel(550724640469942285).send(embed=embed)
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
@@ -140,7 +139,7 @@ class Events(commands.Cog):
         kiirhato = False
         if reaction.message.attachments:
             try:
-                #attachment_link = reaction.message.attachments[0]['url']
+                # attachment_link = reaction.message.attachments[0]['url']
                 attachment_link = reaction.message.attachments[0].url
                 kiirando_uzenet = (f"{user.name} has removed {reaction.emoji} from" +
                     f" {reaction.message.author}'s message in channel {reaction.message.channel.mention}" +
@@ -152,12 +151,12 @@ class Events(commands.Cog):
                 pass
         else:
             kiirando_uzenet = (f"{user.name} has removed {reaction.emoji} from {reaction.message.author}'s" +
-                f" message in channel {reaction.message.channel.mention} from the following message:" + 
+                f" message in channel {reaction.message.channel.mention} from the following message:" +
                 f" {reaction.message.content}")
             kiirhato = True
         embed = discord.Embed(
-            title='``{}:`` '.format(now), 
-            description='{}'.format(kiirando_uzenet), 
+            title=f"``{now}:`` ",
+            description=f"{kiirando_uzenet}",
             colour=discord.Colour.dark_teal()
         )
         embed.set_thumbnail(url=reaction.message.author.avatar_url)
@@ -178,47 +177,46 @@ class Events(commands.Cog):
                     and (before.self_deaf == after.self_deaf)
                     and (before.self_mute == after.self_mute)
                     and (member.id != 549654750585421825)))
-                if ((before.channel is None) and (after.channel.guild.id == 399595937409925140) 
-                    and (before.deaf == after.deaf) and (before.mute == after.mute) 
-                    and (before.self_deaf == after.self_deaf) and (before.self_mute == after.self_mute) 
+                if ((before.channel is None) and (after.channel.guild.id == 399595937409925140)
+                    and (before.deaf == after.deaf) and (before.mute == after.mute)
+                    and (before.self_deaf == after.self_deaf) and (before.self_mute == after.self_mute)
                     and (member.id != 549654750585421825)):
-                        uzenet = '**{}** has just joined **{}**'.format(member.name, after.channel.name)
+                        uzenet = f"**{member.name}** has just joined **{after.channel.name}**"
                         kiirhato = True
-                elif ((before.channel.guild.id == 399595937409925140) and (after.channel is None) and 
-                    (nem_mute_az_event_oka == True)):
-                        uzenet = '**{}** has disconnected from **{}**'.format(member.name, before.channel.name)
+                elif ((before.channel.guild.id == 399595937409925140) and (after.channel is None)
+                    and nem_mute_az_event_oka):
+                        uzenet = f"**{member.name}** has disconnected from **{before.channel.name}**"
                         kiirhato = True
-                elif ((before.channel.guild.id != 399595937409925140) and 
-                    (after.channel.guild.id == 399595937409925140) and (nem_mute_az_event_oka == True)):
-                        uzenet = '**{}** has just joined **{}**'.format(member.name, after.channel.name)
+                elif ((before.channel.guild.id != 399595937409925140) and
+                    (after.channel.guild.id == 399595937409925140) and nem_mute_az_event_oka):
+                        uzenet = f"**{member.name}** has just joined **{after.channel.name}**"
                         kiirhato = True
-                elif ((before.channel.guild.id == 399595937409925140) 
-                    and (after.channel.guild.id == 399595937409925140) 
+                elif ((before.channel.guild.id == 399595937409925140)
+                    and (after.channel.guild.id == 399595937409925140)
                     and (nem_mute_az_event_oka == True)):
-                        uzenet = (f'**{member.name}** has moved from **{before.channel.name}** to' +
-                            f' **{after.channel.name}**')
+                        uzenet = (f"**{member.name}** has moved from **{before.channel.name}** to" +
+                            f" **{after.channel.name}**")
                         kiirhato = True
                 if kiirhato:
                     embed = discord.Embed(
-                        title='``{}:`` '.format(now), 
-                        description='{}'.format(uzenet), 
+                        title=f"``{now}:`` ",
+                        description=f"{uzenet}",
                         colour=discord.Colour.blue()
                     )
                     embed.set_thumbnail(url=member.avatar_url)
-                    embed.set_author(name=member.name + ' has updated their VoiceState', 
-                        icon_url=member.avatar_url)
+                    embed.set_author(name=member.name + " has updated their VoiceState", icon_url=member.avatar_url)
                     await self.bot.get_channel(550724640469942285).send(embed=embed)
         except Exception as e:
             await self.bot.get_channel(550724640469942285).send(f"on_voice_state_update error: {e}")
 
-    #does work
+    # does work
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
         now = datetime.datetime.now()
         try:
             embed = discord.Embed(
-                title='``{}:`` '.format(now),
-                description='{}'.format(":interrobang: Command error: {}".format(error)),
+                title=f"``{now}:`` ",
+                description=f":interrobang: Command error: {error}",
                 colour=discord.Colour.from_rgb(255, 0, 13)
             )
             embed.set_thumbnail(url=ctx.message.author.avatar_url)
--- main.py.a2r.py
+++ main.py.a2r.py
@@ -1,16 +1,16 @@
-#music cog nem lesz automatikusan hozzáadva
+# music cog nem lesz automatikusan hozzáadva
 import datetime
 import random
 import asyncio
 import discord
 from discord.ext import commands
 
-TOKEN = 'token' # git verzió miatt csak egy sztringet kap, futáshoz meg kell adni a Discord Dev oldalon található tokent
+TOKEN = 'token'  # git verzió miatt a tokent kitöröltem, futáshoz meg kell adni a Discord Dev oldalon található tokent
 description = 'Szédületes bot in Python'
 bot = commands.Bot(command_prefix='?', description=description)
 new_Version = False
 major_version_change = False
-#a main mappáján belül kell egy mappának lennie, ami a cogokat tartalmazza, ezt előtegként hozzá kell illeszteni
+# a main mappáján belül kell egy mappának lennie, ami a cogokat tartalmazza, ezt előtagként hozzá kell illeszteni
 extensions = [
     'cogs.adminonly',
     'cogs.events',
@@ -20,16 +20,17 @@ extensions = [
     'cogs.musicSajat'
 ]
 
+
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='commands || ?help'))
-    if new_Version == True:
+    if new_Version:
         await bot.get_channel(549709362206081076).send(":loudspeaker: @here Hey, I've just been updated!" +
             " Type '?changelog' to see what's :new:! ")
-    if major_version_change == True:
+    if major_version_change:
         await bot.get_channel(399595938013773824).send(":loudspeaker: Hey everyone!" +
             "\nI'm making my way out of alpha! This means most of the bugs have been fixed, " +
-                "but you still might experience some issues. If you discover any bugs, please " +
+                "but you might still experience some issues. If you discover any bugs, please " +
                 "report them by using the ``?bug`` command." +
             "\nIf you need help with the commands, please type ``?help``." +
             "\nType ``?changelog`` to see what's new"
@@ -39,10 +40,11 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    if (not hasattr(bot, 'uptime')):
+    if not hasattr(bot, 'uptime'):
         bot.uptime = datetime.datetime.utcnow()
 
-@bot.command(aliases = ['uotime', 'ut', 'upt'])
+
+@bot.command(aliases=['uotime', 'ut', 'upt'])
 async def uptime(ctx):
     elso = True
     await ctx.message.delete()
@@ -95,6 +97,7 @@ async def uptime(ctx):
                 file = open("/srv/shared/Simi/programozas/discordbot/0rekord.txt", "w")
                 file.write(f'{osszesen_masodpercben}\n{uptime}')
                 file.close()
+                fajlban_tarolt = 1
             if osszesen_masodpercben > fajlban_tarolt:
                 file = open("/srv/shared/Simi/programozas/discordbot/0rekord.txt", "w")
                 file.write(f'{osszesen_masodpercben}\n{uptime}')
@@ -120,7 +123,8 @@ def get_human_readable_uptime_diff(start_time):
         fmt = '{h} hours, {m} minutes, and {s} seconds'
     return fmt.format(d=days, h=hours, m=minutes, s=seconds)
 
-@bot.command(aliases = ['l'])
+
+@bot.command(aliases=['l'])
 async def load(ctx, extension):
     'Admin-only command.'
     if ctx.author.id == 358992693453652000:
@@ -134,14 +138,14 @@ async def load(ctx, extension):
         await ctx.send('You cannot do that. :no_entry:')
 
 
-@bot.command(aliases = ['uload', 'ul'])
+@bot.command(aliases=['uload', 'ul'])
 async def unload(ctx, extension):
     'Admin-only command.'
     if ctx.author.id == 358992693453652000:
         try:
             await ctx.message.delete()
             bot.unload_extension(extension)
-            await ctx.send('Unloaded {} :white_check_mark: '.format(extension), delete_after = 5)
+            await ctx.send(f'Unloaded {extension} :white_check_mark: ', delete_after=5)
         except Exception as error:
             await ctx.send("{} can't be unloaded. [{}]".format(extension, error))
     else:
